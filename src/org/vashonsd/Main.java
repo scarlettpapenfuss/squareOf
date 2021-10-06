96fa2ead91b388be6a1c02a78cb11a0bd007bf89
scarlettpapenfuss <papenfuss.scarlett@vashonsd.org>
2021-10-06 10:43:45 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,12 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(squareOf(6));
+    }
+
+    public static int squareOf(int n) {
+        return n * n;
+    }
+}
